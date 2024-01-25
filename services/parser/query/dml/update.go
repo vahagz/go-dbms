feccd5f395646da1b5d3044e172a782e5e9567dc
vahagz <vahag.zargaryan2002@gmail.com>
2024-01-25 17:49:51 +0400

    update implemented

--- services/parser/query/dml/update.go
+++ services/parser/query/dml/update.go
@@ -1,14 +1,14 @@
 package dml
 
 import (
-	"go-dbms/pkg/statement"
 	"go-dbms/services/parser/query"
 )
 
 type QueryUpdate struct {
 	query.Query
-	DB     string                   `json:"db"`
-	Table  string                   `json:"table"`
-	Values dataMap                  `json:"values"`
-	Where  statement.WhereStatement `json:"where"`
+	DB         string      `json:"db"`
+	Table      string      `json:"table"`
+	Values     dataMap     `json:"values"`
+	Where      *where      `json:"where"`
+	WhereIndex *whereIndex `json:"where_index"`
 }
