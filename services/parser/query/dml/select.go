ce1e81f5c941403c0da6b3c478e1b41e9e6c3081
vahagz <vahag.zargaryan2002@gmail.com>
2024-02-01 17:26:50 +0400

    where section parser in select queries

--- services/parser/query/dml/select.go
+++ services/parser/query/dml/select.go
@@ -2,9 +2,11 @@ package dml
 
 import (
 	"encoding/json"
+	"text/scanner"
+
+	"go-dbms/pkg/statement"
 	"go-dbms/pkg/types"
 	"go-dbms/services/parser/query"
-	"text/scanner"
 
 	"github.com/pkg/errors"
 )
@@ -52,15 +54,10 @@ func (qs *QuerySelect) Parse(s *scanner.Scanner) (err error) {
 		qs.parseWhereIndex(s)
 	}
 
-	// switch word {
-	// 	case ";": break
-	// 	case "WHERE_INDEX":
-	// 	case "WHERE":
-	// 	case "GROUP_BY":
-	// 	case "HAVING":
-	// 	case "ORDER_BY":
-	// 	case "LIMIT":
-	// }
+	word = s.TokenText()
+	if word == "WHERE" {
+		qs.parseWhere(s)
+	}
 
 	return nil
 }
@@ -138,7 +135,16 @@ func (qs *QuerySelect) parseWhereIndex(s *scanner.Scanner) {
 
 	qs.WhereIndex = &whereIndex{}
 	qs.WhereIndex.Name = word
-	qs.WhereIndex.FilterStart = parseWhereIndexFilter(s)
+	qs.WhereIndex.FilterStart = &indexFilter{}
+	col, op, val := parseWhereFilter(s, false)
+	var valInt interface{}
+	if err := json.Unmarshal([]byte(val), &valInt); err != nil {
+		panic(err)
+	}
+	qs.WhereIndex.FilterStart.Operator = op
+	qs.WhereIndex.FilterStart.Value = map[string]types.DataType{
+		col: types.ParseJSONValue(valInt),
+	}
 
 	tok = s.Scan()
 	word = s.TokenText()
@@ -148,20 +154,37 @@ func (qs *QuerySelect) parseWhereIndex(s *scanner.Scanner) {
 	}
 
 	if word == "AND" {
-		qs.WhereIndex.FilterEnd = parseWhereIndexFilter(s)
+		qs.WhereIndex.FilterEnd = &indexFilter{}
+		col, op, val := parseWhereFilter(s, false)
+		var valInt interface{}
+		if err := json.Unmarshal([]byte(val), &valInt); err != nil {
+			panic(err)
+		}
+		qs.WhereIndex.FilterEnd.Operator = op
+		qs.WhereIndex.FilterEnd.Value = map[string]types.DataType{
+			col: types.ParseJSONValue(valInt),
+		}
 	}
+
+	s.Scan()
 }
 
-func parseWhereIndexFilter(s *scanner.Scanner) *indexFilter {
-	f := &indexFilter{}
+func parseWhereFilter(s *scanner.Scanner, firstScanned bool) (col, op, val string) {
+	var tok rune
+	var word string
+	var isKW bool
 
-	tok := s.Scan()
-	word := s.TokenText()
-	_, isKW := keyWords[word]
-	if tok == scanner.EOF || isKW {
-		panic(ErrSyntax)
+	if !firstScanned {
+		tok = s.Scan()
+		word = s.TokenText()
+		_, isKW = keyWords[word]
+		if tok == scanner.EOF || isKW {
+			panic(ErrSyntax)
+		}
+		col = word
+	} else {
+		col = s.TokenText()
 	}
-	col := word
 	
 	tok = s.Scan()
 	word = s.TokenText()
@@ -169,7 +192,7 @@ func parseWhereIndexFilter(s *scanner.Scanner) *indexFilter {
 	if tok == scanner.EOF || !isLO{
 		panic(ErrSyntax)
 	}
-	op := word
+	op = word
 	
 	if s.Peek() == '=' {
 		op += "="
@@ -177,19 +200,59 @@ func parseWhereIndexFilter(s *scanner.Scanner) *indexFilter {
 	}
 
 	tok = s.Scan()
-	word = s.TokenText()
+	val = s.TokenText()
 	if tok == scanner.EOF{
 		panic(ErrSyntax)
 	}
 
-	var val interface{}
-	if err := json.Unmarshal([]byte(word), &val); err != nil {
-		panic(err)
+	return col, op, val
+}
+
+func (qs *QuerySelect) parseWhere(s *scanner.Scanner) {
+	qs.Where = (*where)(parseWhere(s))
+}
+
+func parseWhere(s *scanner.Scanner) (*statement.WhereStatement) {
+	var logOp string
+	sttmnts := []*statement.WhereStatement{}
+
+	for {
+		tok := s.Scan()
+		word := s.TokenText()
+		_, isKW := keyWords[word]
+		if tok == scanner.EOF {
+			panic(ErrSyntax)
+		} else if word == "(" {
+			sttmnts = append(sttmnts, parseWhere(s))
+		} else if word == ")" || word == ";" || isKW {
+			break
+		} else if _, ok := logicalOperators[word]; ok {
+			logOp = word
+		} else {
+			col, op, val := parseWhereFilter(s, true)
+			var valInt interface{}
+			if err := json.Unmarshal([]byte(val), &valInt); err != nil {
+				panic(err)
+			}
+
+			sttmnts = append(sttmnts, &statement.WhereStatement{
+				Statement: &statement.Statement{
+					Col: col,
+					Op:  op,
+					Val: types.ParseJSONValue(valInt),
+				},
+			})
+		}
 	}
 
-	f.Operator = op
-	f.Value = map[string]types.DataType{
-		col: types.ParseJSONValue(val),
+	if logOp == "AND" {
+		return &statement.WhereStatement{
+			And: sttmnts,
+		}
+	} else if logOp == "OR" {
+		return &statement.WhereStatement{
+			Or: sttmnts,
+		}
 	}
-	return f
+	return sttmnts[0]
 }
